42266148e9528fb5e7d43212c11bfb8cf4afb0ce
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2026-02-28 18:24:47 +0630

    feat: Update flutter_mmcalendar dependency to 3.3.0 across all packages and adjust import statements.

--- packages/features/calendar/lib/src/presentation/widgets/date_cell.dart
+++ packages/features/calendar/lib/src/presentation/widgets/date_cell.dart
@@ -1,7 +1,6 @@
 import 'package:shared_core/shared_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mmcalendar/flutter_mmcalendar.dart'
-    hide CompactMoonPhaseIndicator, MoonPhaseIndicator;
+import 'package:flutter_mmcalendar/flutter_mmcalendar.dart';
 import 'package:shared_ui_kit/shared_ui_kit.dart';
 
 class DateCell extends StatefulWidget {
